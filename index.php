e4512bd4f9beb690a2c6ee12168cf32aa67c6005
Prasetyo Wibowo <indocoffee@gmail.com>
2017-02-12 12:13:36 -0500

    added portfolio website files, and remove index.html

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+include('header.php'); ?>
+
+	<div id="wrapper3">
+		<div class="mainWrap">
+			<a name="myWork"></a>
+			<h2>My Work</h2>
+			<p>Below are some samples of my finished projects.</p>
+			<h3>PHP Web Applications</h3>
+			<p>Screenshots of my PHP web applications are listed below. <strong><em>Source code and demo available on request.</em></strong></p>
+			<div class="bucketPHP">
+				<img src="images/portfolio/phpapp/compressorsizing.jpg">
+				<p><strong>Compressor Sizing program.</strong><br> The program is used as the company's internal tool to determine which compressor is best recommended based on input and data from user against a database.</p>
+			</div>
+
+			<div class="bucketPHP">
+				<img src="images/portfolio/phpapp/pcbquote.jpg">
+				<p><strong>PCB Quoting Online.</strong><br> Enable users to register, create PCB sizing entries, load, edit and delete. In the end the user can get a realtime quote. They can also submit their saved quote directly to the company.</p>
+			</div>
+
+			<div class="bucketPHP">
+				<img src="images/portfolio/phpapp/pumpsizing.jpg">
+				<p><strong>Pump Sizing Program.</strong><br> The company needs a program to automate the creation of recommended upgrades on their Pump Systems. This program does not only that, but email the recommended upgrades and reports in HTML email with pdf attachments. </p>
+			</div>
+
+			<div class="bucketPHP">
+				<img src="images/portfolio/phpapp/pumpsystem.jpg">
+				<p><strong>Pump Systems Builder.</strong><br> The company needs a program available for their distributor to generate quotes and pump system specs online. It can also save, edit, delete and submit the pump system online. </p>
+			</div>
+
+			<p class="dwClear">&nbsp;</p>
+			<h3>Websites</h3>
+			<p>Screenshots of websites I finished are listed below. <strong><em>Click</em></strong> on the images to go to the live websites.</p>
+			<div class="bucket">
+				<a href="http://www.macmachine.com/" target="_blank"><img src="images/portfolio/small/macmachine.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.ogontz.com/" target="_blank"><img src="images/portfolio/small/ogontz.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.discountabrasives.com/" target="_blank"><img src="images/portfolio/small/discount-abrasives.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.americancrane.com/" target="_blank"><img src="images/portfolio/small/americancrane.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.cachemspecialties.com/" target="_blank"><img src="images/portfolio/small/cachem.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://leica.imillermicroscopes.com/" target="_blank"><img src="images/portfolio/small/imiller.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.hedwin.com/" target="_blank"><img src="images/portfolio/small/hedwin.png"></a>
+			</div>
+
+
+			<div class="bucket">
+				<a href="http://www.abtecinc.com/" target="_blank"><img src="images/portfolio/small/abtecinc.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.bassettinc.com/" target="_blank"><img src="images/portfolio/small/bassett.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.buckalgonquin.com/" target="_blank"><img src="images/portfolio/small/buck.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.doucetteindustries.com/" target="_blank"><img src="images/portfolio/small/doucette.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.mortonmachine.com/" target="_blank"><img src="images/portfolio/small/mortonmachine.png"></a>
+			</div>
+			<div class="bucket">
+				<a href="http://www.mrspring.com/" target="_blank"><img src="images/portfolio/small/mrspring.png"></a>
+			</div>
+
+			<div class="bucket">
+				<a href="http://www.xode.com/" target="_blank"><img src="images/portfolio/small/xode.png"></a>
+			</div>
+
+		</div>
+	</div><!-- wrapper 3 -->
+
+
+<?php
+include ('footer.php');
---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form action="/" method="get">
+    <fieldset>
+        <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
+        <input type="submit" value="Search" alt="Search" class="btn btn-info" />
+    </fieldset>
+</form>
