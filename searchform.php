2ecf839591ec751192bae08867293f4d186bba25
Prasetyo Wibowo <indocoffee@gmail.com>
2017-02-12 16:10:32 -0500

    cleanup and change unnecessary/neccessary files

--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form action="/" method="get">
-    <fieldset>
-        <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
-        <input type="submit" value="Search" alt="Search" class="btn btn-info" />
-    </fieldset>
-</form>
