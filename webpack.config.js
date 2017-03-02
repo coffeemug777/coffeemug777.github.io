f69fcbaadaa5b1e34b64fc84f136ba7f483aab0e
Prasetyo Wibowo <indocoffee@gmail.com>
2017-03-01 22:43:35 -0500

    Add: entries for less file handling

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	devtool: 'source-map',
+	entry: [
+		'./src/index.js',
+		'./src/styles.less',
+	],
 	output: {
 		filename: './dist/bundle.js'
 	},
@@ -14,7 +19,19 @@ module.exports = {
 				query: {
 					presets: ['es2015', 'react']
 				}
-			}
+			},
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract(
+                    // activate source maps via loader query 
+                    'css-loader?sourceMap!' +
+                    'less-loader?sourceMap'
+                )
+            }
 		]
-	}
+	},
+	plugins: [
+		// extract CSS into separate file
+		new ExtractTextPlugin('./css/styles.css')
+	]	
 };
